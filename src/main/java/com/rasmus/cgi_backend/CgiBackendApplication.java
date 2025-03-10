444fc24dbfa641eadbc56a684f94b188476492d3
Rasmus Klaus <rasmusklaus6@gmail.com>
2025-03-10 16:27:54 +0200

    first commit

---
+++ src/main/java/com/rasmus/cgi_backend/CgiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rasmus.cgi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmus/cgi_backend/CgiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmus.cgi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
