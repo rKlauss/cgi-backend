c900e077c324f75045d2c78c4e425971b1932434
Rasmus Klaus <rasmusklaus6@gmail.com>
2025-03-11 14:27:12 +0200

    remove import

--- src/main/java/com/rasmus/cgi_backend/model/Flight.java
+++ src/main/java/com/rasmus/cgi_backend/model/Flight.java
@@ -1,6 +1,5 @@
 package com.rasmus.cgi_backend.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
