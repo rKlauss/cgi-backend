8106dee623bd055ad221baef5ab9642c20009fc4
Rasmus Klaus <rasmusklaus6@gmail.com>
2025-03-17 15:00:06 +0200

    tests

--- src/test/java/com/rasmus/cgi_backend/CgiBackendApplicationTests.java
+++ src/test/java/com/rasmus/cgi_backend/CgiBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.rasmus.cgi_backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CgiBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
